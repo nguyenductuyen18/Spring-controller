b689495565756e15cccffa8c58e9edeee469827b
nguyenductuyen18 <nguyenductuyen2k4@gmail.com>
2023-12-28 11:02:24 +0700

    ThucHanhTuan4

---
+++ src/main/java/com/example/demo17/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.example.demo17.service;
+
+import com.example.demo17.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
